5f7bad880669115e8f0c6b96d4dab8f677d0e621
kasunkv <kvkrusl@gmail.com>
2016-11-15 20:07:50 +0530

    Initial Commit with project and tests

---
+++ AzureContinuousDeliveryGitHubDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureContinuousDeliveryGitHubDemo.Startup))]
+namespace AzureContinuousDeliveryGitHubDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
